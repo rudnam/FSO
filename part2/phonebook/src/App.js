c71707f358a786dff8d215b7d950bc421c20fa3d
rudnam <francisalbarracin2@gmail.com>
2023-03-28 14:03:24 +0800

    Do The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,6 +8,10 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     setPersons([...persons].concat({ name: newName}))
     setNewName('')
   }
