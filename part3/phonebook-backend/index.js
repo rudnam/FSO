d2351f5973afabb505d4cd145850d3caaf6782f2
rudnam <francisalbarracin2@gmail.com>
2023-05-02 14:49:55 +0800

    3.5: Phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -53,6 +53,32 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    const max = 1000000
+    // 0 <= Id < max
+    return Math.floor(Math.random() * max)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    
+    persons = persons.concat(person)
+    
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
