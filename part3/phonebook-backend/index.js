5f0e6fe81686ffdef84b2655d63ec55e3c698e7c
rudnam <francisalbarracin2@gmail.com>
2023-05-02 14:53:20 +0800

    3.6: Phonebook backend step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -66,6 +66,14 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'name missing'
         })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
     }
 
     const person = {
