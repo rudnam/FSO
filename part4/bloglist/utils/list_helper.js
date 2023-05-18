ea2a28dcacf025f8456fa56d84332bec9efdd031
rudnam <francisalbarracin2@gmail.com>
2023-05-18 17:35:00 +0800

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item.likes;
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
