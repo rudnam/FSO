1c23b87e74f2afb5ef487c7ec2eac3a719f2976b
rudnam <francisalbarracin2@gmail.com>
2023-05-22 17:54:44 +0800

    4.20*: bloglist expansion, step8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '');
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 });
@@ -23,7 +15,7 @@ blogsRouter.post('/', async (request, response) => {
     return;
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     response.status(401).json({ error: 'token invalid' });
     return;
