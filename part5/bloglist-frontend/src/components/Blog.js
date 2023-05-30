4834f5fc6084e35d56db69c3a8949dcee2fca9d5
rudnam <francisalbarracin2@gmail.com>
2023-05-30 10:47:48 +0800

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,13 +1,43 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/function-component-definition */
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-    {' '}
-    {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {`${blog.title} ${blog.author}`}
+      <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+      <button style={showWhenVisible} onClick={toggleVisibility}>hide</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        {`likes ${blog.likes}`}
+        <button onClick={() => console.log('like')}>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
