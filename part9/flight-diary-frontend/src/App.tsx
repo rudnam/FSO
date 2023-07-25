127647faf8098ec6917b951c3baea75c4c6eb712
rudnam <francisalbarracin2@gmail.com>
2023-07-25 15:31:12 +0800

    Exercise 9.17

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,19 +1,70 @@
 import { DiaryEntry } from "./types";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { createEntry, getAllEntries } from "./services/diaryService";
+import { parseVisibility, parseWeather } from "./utils";
 
 const App = () => {
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
-    axios
-      .get<DiaryEntry[]>("http://localhost:3001/api/diaries")
-      .then((response) => {
-        setEntries(response.data);
-      });
+    getAllEntries().then((data) => setEntries(data));
   }, []);
+
+  const addEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createEntry({
+      date,
+      visibility: parseVisibility(visibility),
+      weather: parseWeather(weather),
+      comment,
+    }).then((data) => {
+      setEntries(entries.concat(data));
+    });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
+
   return (
     <div>
+      <form onSubmit={addEntry}>
+        <h2>Add new entry</h2>
+        <div>
+          date
+          <input
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            value={visibility}
+            onChange={(event) => setVisibility(event.target.value)}
+          />
+        </div>
+        <div>
+          weather
+          <input
+            value={weather}
+            onChange={(event) => setWeather(event.target.value)}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Diary entries</h2>
       {entries.map((entry) => (
         <p key={entry.id}>
           <b>{entry.date}</b>
---
+++ part9/flight-diary-frontend/src/utils.ts
@@ -0,0 +1,31 @@
+import { Visibility, Weather } from "./types";
+
+const isString = (text: unknown): text is string => {
+  return typeof text === "string" || text instanceof String;
+};
+
+const isVisibility = (param: string): param is Visibility => {
+  return Object.values(Visibility)
+    .map((v) => v.toString())
+    .includes(param);
+};
+
+export const parseVisibility = (visibility: unknown): Visibility => {
+  if (!isString(visibility) || !isVisibility(visibility)) {
+    throw new Error("Incorrect visibility");
+  }
+  return visibility;
+};
+
+const isWeather = (param: string): param is Weather => {
+  return Object.values(Weather)
+    .map((v) => v.toString())
+    .includes(param);
+};
+
+export const parseWeather = (weather: unknown): Weather => {
+  if (!isString(weather) || !isWeather(weather)) {
+    throw new Error("Incorrect weather");
+  }
+  return weather;
+};
